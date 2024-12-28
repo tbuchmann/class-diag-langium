37b521574fd56d1607f2221388979a34d3d04d16
tbuchmann <thomas.buchmann@gmail.com>
2024-12-28 21:24:30 +0100

    validator + generator extensions

--- src/cli/generator.ts
+++ src/cli/generator.ts
@@ -1,5 +1,5 @@
 //import type { Model } from '../language/generated/ast.js';
-import type { Class } from '../language/generated/ast.js';
+import { type Enumeration, type Class, type Model, type Package, type Interface } from '../language/generated/ast.js';
 import { expandToNode, /*joinToNode,*/ toString } from 'langium/generate';
 import * as fs from 'node:fs';
 import * as path from 'node:path';
@@ -22,7 +22,91 @@ export function generateJavaScript(model: Model, filePath: string, destination: 
     return generatedFilePath;
 }
 */
-export function generateJava(clz: Class, filePath: string, destination: string | undefined): string {
+
+export function generateCode(model: Model, filePath: string, destination: string | undefined): string {
+    /*
+    const data = extractDestinationAndName(filePath, destination);
+    const generatedFilePath = `${path.join(data.destination, data.name)}.java`;
+
+    const fileNode = expandToNode`
+        package ${model.name};
+
+        ${model.packages.map(pkg => `
+            package ${pkg.name} {
+                ${pkg.types.map(type => `
+                    ${type.$type === 'Class' ? `
+                        class ${type.name} {
+                            ${type.features.map(feature => `
+                                ${feature.$type === 'Property' ? `
+                                    ${feature.name} : ${feature.type.name}
+                                ` : feature.$type === 'Operation' ? `
+                                    ${feature.name}(${feature.parameters.map(param => `${param.name} : ${param.type.name}`).join(', ')}) : ${feature.returnType.name}
+                                ` : ''}
+                            `).join('\n')}
+                        }
+                    ` : ''}
+                `).join('\n')}
+            }
+        `).join('\n')}
+    `.appendNewLineIfNotEmpty();
+
+    if (!fs.existsSync(data.destination)) {
+        fs.mkdirSync(data.destination, { recursive: true });
+    }
+    fs.writeFileSync(generatedFilePath, toString(fileNode));
+    return generatedFilePath;
+    */
+   model.packages.forEach(pkg => {
+        pkg.types.forEach(type => {
+            if (type.ref?.$type === 'Class') {
+                generateClass(type.ref, pkg.name, filePath, destination);
+            }
+        });
+   });   
+   return destination || '';
+}
+
+export function generateClassDiagram(pkg: Package, filePath: string, destination: string) : string {
+    const data = extractDestinationAndName(filePath, destination);
+    const generatedFilePath = `${path.join(data.destination, data.name)}.classdiag`;
+
+    let classSet = new Set<Class>();
+    let enumSet = new Set<Enumeration>();
+    let interfaceSet = new Set<Interface>();
+
+    pkg.types.forEach(type => {
+        if (type.ref?.$type === 'Class') {
+            classSet.add(type.ref);
+        } else if (type.ref?.$type === 'Enumeration') {
+            enumSet.add(type.ref);
+        }  else if (type.ref?.$type === 'Interface') {
+            interfaceSet.add(type.ref);
+        }
+    });
+
+    const fileNode = expandToNode`
+        @startuml
+        ${Array.from(classSet).map(clz => `
+            class ${clz.name} 
+        `).join('\n')}
+        ${Array.from(enumSet).map(enm => `
+            enum ${enm.name} 
+        `).join('\n')}
+        ${Array.from(interfaceSet).map(inf => `
+            interface ${inf.name} 
+        `).join('\n')}
+        @enduml
+    `.appendNewLineIfNotEmpty();
+
+    if (!fs.existsSync(data.destination)) {
+        fs.mkdirSync(data.destination, { recursive: true });
+    }
+    fs.writeFileSync(generatedFilePath, toString(fileNode));
+    return generatedFilePath;
+}
+
+
+export function generateClass(clz: Class, pkgName: string, filePath: string, destination: string | undefined): string {
     const data = extractDestinationAndName(filePath, destination);
     const generatedFilePath = `${path.join(data.destination, data.name)}.java`;
 
--- src/language/class-diagram-validator.ts
+++ src/language/class-diagram-validator.ts
@@ -1,5 +1,5 @@
 import type { ValidationAcceptor, ValidationChecks } from 'langium';
-import type { ClassDiagramAstType, Type, Property, Operation } from './generated/ast.js';
+import type { ClassDiagramAstType, Class, Interface, Type, Property, Operation } from './generated/ast.js';
 import type { ClassDiagramServices } from './class-diagram-module.js';
 
 /**
@@ -9,7 +9,7 @@ export function registerValidationChecks(services: ClassDiagramServices) {
     const registry = services.validation.ValidationRegistry;
     const validator = services.validation.ClassDiagramValidator;
     const checks: ValidationChecks<ClassDiagramAstType> = {
-        Class: validator.checkTypeStartsWithCapital,
+        Class: [ validator.checkTypeStartsWithCapital, validator.checkNoCycleInClassInheritance],
         Interface: validator.checkTypeStartsWithCapital,
         DataType: validator.checkTypeStartsWithCapital,
         Enumeration: validator.checkTypeStartsWithCapital,
@@ -51,4 +51,12 @@ export class ClassDiagramValidator {
         }
     }
 
+    checkNoCycleInClassInheritance(c: Class, accept: ValidationAcceptor): void {
+
+    }
+
+    checkNoCycleInInterfaceInheritance(i: Interface, accept: ValidationAcceptor): void {
+
+    }
+    
 }
